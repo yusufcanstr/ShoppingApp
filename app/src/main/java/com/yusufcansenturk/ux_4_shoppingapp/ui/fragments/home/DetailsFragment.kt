e1f1c6117941e07f69757fac9590caf69e294dc1
Yusufcan Senturk <yusufcansenturk10@gmail.com>
2023-05-11 21:41:23 +0300

    screen showing product details was designed

---
+++ app/src/main/java/com/yusufcansenturk/ux_4_shoppingapp/ui/fragments/home/DetailsFragment.kt
@@ -0,0 +1,32 @@
+package com.yusufcansenturk.ux_4_shoppingapp.ui.fragments.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.yusufcansenturk.ux_4_shoppingapp.R
+import com.yusufcansenturk.ux_4_shoppingapp.databinding.FragmentDetailsBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DetailsFragment : Fragment() {
+
+    private var _binding: FragmentDetailsBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
